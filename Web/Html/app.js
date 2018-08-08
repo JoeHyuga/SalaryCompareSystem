746a65154c04679e484dd7eb261ef318c3610756
JoeHyuga <tongtng661@qq.com>
2018-08-08 22:34:51 +0800

    添加导航栏

--- Web/Html/app.js
+++ Web/Html/app.js
@@ -1,13 +1,18 @@
 ﻿var app = angular.module('MyApp', ['ngRoute']);
 
 app.controller('CompanyController', function ($scope, $route) { $scope.$route = $route; })
+app.controller('CompanyEditController', function ($scope, $route) { $scope.$route = $route; })
     .config(function ($routeProvider) {
         $routeProvider.
-            when('/CompanyInfo', {
+            when('/companyInfo', {
                 templateUrl: 'companyInfo.html',
                 controller: 'CompanyController'
             }).
+            when('/companyInfoEdit', {
+                templateUrl: 'companyInfoEdit.html',
+                controller: 'CompanyEditController'
+            }).
             otherwise({
-                redirectTo: '/CompanyInfo'
+                redirectTo: '/companyInfo'
             });
     });
---
+++ Web/Html/companyInfoEdit.js
@@ -0,0 +1,3 @@
+﻿app.controller('CompanyEditController', function ($scope) {
+
+});
